d6c4707ccf2f1b0864b9d96d11c92e45ac77d74e
gucong <gucong@gmail.com>
2013-11-19 13:55:28 +0800

    修复IE9不加载H5F的问题；修复IE10、IE11中select标签不能错误提示的问题；添加登陆注册模块表单验证

--- plugins/jQuery.jmvalidate/js/jquery.jmvalidate.js
+++ plugins/jQuery.jmvalidate/js/jquery.jmvalidate.js
@@ -10,15 +10,13 @@
 (function($){
 	var input = $("<input>")[0],
 		support = "validity" in input && "checkValidity" in input,
+		doc = document,
 		initInvalid;	
 
 	//如果浏览器不支持HTML5,则加载h5f.js
-	(function(d){
-		if(!support){
-			var scrNode = $(d.scripts ? d.scripts[d.scripts.length - 1] : "script:last");
-			scrNode.attr("src", scrNode.attr("data-h5f"));
-		}
-	})(document);
+	if(!support){
+		doc.writeln("<script src=\""+ $(doc.scripts ? doc.scripts[doc.scripts.length - 1] : "script:last").attr("data-h5f")+"\"></script>");
+	}
 
 	//回调的触发
 	function validityCall(opt, e){
@@ -40,37 +38,51 @@
 		}
 	}
 
-	//支持HTML5验证的浏览器，为了去掉默认样式，阻止全部浏览器默认行为；
-	initInvalid = support ? function(form, opt){
-		function prevent(node){
-			$(node).bind("invalid", function(e){
-				e.preventDefault();
-				validityCall(opt, e);
-			});
+	function initInvalid(form, opt){
+
+		//invalid事件处理函数
+		function invalidFn(e){
+			//阻止事件冒泡
+			e.stopPropagation();
+			//为了去掉默认样式，阻止全部浏览器默认行为；
+			e.preventDefault();
+			validityCall(opt, e);
 		}
+
+		//绑定事件处理函数
+		function prevent(node){
+			$(node).bind("invalid", invalidFn);
+		}
+
+		//不支持HTML5验证的浏览器，使用H5F
+		if(!support){
+			H5F.setup(form, opt);
+		}
+
 		//将现有表单元素去除默认行为
 		prevent(form.elements);
-		$(form).bind("DOMNodeInserted", function(e){
-			var target = e.target;
-			if("validity" in target && "checkValidity" in target){
-				//将动态添加的元素去除默认行为
-				prevent(target);
+		prevent(form);
+
+		if(support || doc.createEvent) {
+
+			form = $(form).bind("DOMNodeInserted", function(e){
+				var target = e.target;
+				if("validity" in target && "checkValidity" in target){
+					//将动态添加的元素去除默认行为
+					prevent(target);
+				}
+			});
+
+			if(support){
+				//由于阻止了默认事件，需要重新模拟焦点行为
+				form.delegate(":submit", "click", function(e){
+					var invalid = this.form.querySelector(":invalid");
+					invalid && invalid.focus();
+				}).bind("change", function(e){
+					toggleClass(e.target, opt);
+				});
 			}
-		}).delegate(":submit", "click", function(e){
-			//由于阻止了默认事件，需要重新模拟焦点行为
-			var invalid = this.form.querySelector(":invalid");
-			invalid && invalid.focus();
-		}).bind("change", function(e){
-			toggleClass(e.target, opt);
-		});
-	} : function(form, opt){
-		//不支持HTML5验证的浏览器，使用H5F
-		opt.jQuery = $;
-		H5F.setup(form, opt);
-		$(form).bind("invalid", function(e){
-			e.stopImmediatePropagation();
-			validityCall(opt, e);
-		});
+		}
 	};
 
 	/*表单验证公共组件*/
@@ -86,9 +98,12 @@
 			for(var i in opt.events){
 				this.delegate(i, opt.events[i], function(e){
 					var input = e.target;
-					if(input.checkValidity && input.checkValidity()){
-						validityCall(opt, e);
-					}
+					//狗日的IE10、11在change事件发生时select标签的validity还未更新，所以延迟
+					window.setTimeout(function(){
+						if(input.checkValidity && input.checkValidity()){
+							validityCall(opt, e);
+						}
+					}, 1);
 				});
 			}
 		}
@@ -125,5 +140,40 @@
 				"*": "change"
 			}
 		}, opt));
+	};
+
+
+	/*注册流程表单验证*/
+	function signValidate(e){
+		var me = e.target,
+		v = me.validity,
+		c = $(me).closest(".textbox_ui");
+		c.find(".valid").toggle(v.valid);
+		c.find(".required").toggle(v.valueMissing);
+		c.find(".custom").toggle(v.customError).html(v.customError ? me.validationMessage : "");
+		c.find(".format").toggle(!v.valid && !v.valueMissing && !v.customError);
+		c.find(".invalid").toggle(!v.valid);
+		c.toggleClass("error_ui", !v.valid);
+	}
+
+	/*注册流程表单验证*/
+	$.fn.signvalidate = function(opt){
+		var jqobj = this;
+		(jqobj.valuechange || jqobj.change).call(jqobj, function(e){
+			var v = e.target.validity;
+			if(v && v.valid){
+				$(e.target).closest(".textbox_ui").removeClass("error_ui");
+			}
+		}).h5validity($.extend({
+			validity: signValidate,
+			events: {
+				"*": "change"
+			}
+		}, opt)).delegate(":submit", "click", function(e){
+			if($(e.currentTarget).find(".loading:visible").length){
+				return false;
+			}
+		});
+		return jqobj;
 	};
 })(jQuery);
